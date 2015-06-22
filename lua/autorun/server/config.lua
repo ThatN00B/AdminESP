1a1166fb47e7e7d880573f677aaa83fe3047dfbe
ThatN00B <nopenopenopenope123@gmail.com>
2015-06-21 20:59:06 -0700

    Delete config.lua

--- lua/autorun/server/config.lua
+++
@@ -1,9 +0,0 @@
-AESP = AESP or {}
-AESP.Config = AESP.Config or {
-     ShowPing = true,
-     ShowHealth = true,
-     ShowArmor = true,
-     ShowGroup = true,
-     ShowJob = true,
-     ShowMoney = false, -- Only set to true if using DarkRP
-}
