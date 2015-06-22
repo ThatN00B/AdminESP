1f018d5e26460d7a43ecbb2b1dad61e70824503d
Jake <vegihon@aol.com>
2015-06-21 22:29:11 -0400

    Create config.lua

---
+++ lua/autorun/server/config.lua
@@ -0,0 +1,9 @@
+AESP = AESP or {}
+AESP.Config = AESP.Config or {
+     ShowPing = true,
+     ShowHealth = true,
+     ShowArmor = true,
+     ShowGroup = true,
+     ShowJob = true,
+     ShowMoney = true, -- Only set to true if using DarkRP
+}
